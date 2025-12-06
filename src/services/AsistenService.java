69a9878d5a0d77718d3d0a53018b025a46300ca7
unknown <6182201059@student.unpar.ac.id>
2025-12-06 14:57:40 +0700

    Backend Laporan Event Asisten

--- src/services/AsistenService.java
+++ src/services/AsistenService.java
@@ -13,9 +13,45 @@ public class AsistenService {
     private final KlienRepository klienRepo = new KlienRepository();
     private final EventRepository eventRepo = new EventRepository();
 
+    
+    public List<Klien> getDaftarKlien() {
+        return klienRepo.findAll();
+    }
+
+    public boolean tambahEvent(String namaEvent, String tanggal, int undangan,
+                               double budget, int idJenisEvent, int idKlien) {
+
+        int idAsisten = UserSession.getUserId();
+
+        Event event = new Event();
+        event.setNama(namaEvent);
+        event.setTanggal(Date.valueOf(tanggal));
+        event.setJumlahUndangan(undangan);
+        event.setStatus("On Progress");
+        event.setBudget(budget);
+        event.setIdJenisEvent(idJenisEvent);
+        event.setIdKlien(idKlien);
+        event.setIdAsisten(idAsisten);
+
+        return eventRepo.insert(event);
+    }
+
+    public boolean alokasikanVendor(int idEvent, int idVendor, double hargaDealing) {
+        return eventRepo.alokasikanVendor(idEvent, idVendor, hargaDealing);
+    }
+
+    public boolean updateStatus(int idEvent, String statusBaru) {
+        return eventRepo.updateStatus(idEvent, statusBaru);
+    }
+
     public boolean tambahKlien(String nama, String alamat, String telp, String email) {
         Klien k = new Klien(0, nama, alamat, telp, email);
         return klienRepo.insert(k);
+    }
+
+    public List<Event> getEventByAsisten() {
+    int id = UserSession.getUserId();
+    return eventRepo.getEventsByAsisten(id);
     }
 
     public List<Klien> getKlienByAsisten() {
@@ -23,4 +59,8 @@ public class AsistenService {
         return eventRepo.getKlienByAsisten(id);
     }
 
+    public List<String[]> getDetailKlienEvent() {
+        int id = UserSession.getUserId();
+        return eventRepo.getDetailKlienEventByAsisten(id);
+    }
 }
